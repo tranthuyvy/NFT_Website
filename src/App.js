01a482a4e2b0acd9397cd57a1a46c8379ef00a3e
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2024-01-14 23:02:54 +0700

    token

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { useEffect } from 'react'
 import './App.css';
 import Login from './components/Login.jsx'
+import { useStateProvider } from './utils/StateProvider.js'
+import { reducerCases } from './utils/Constants.js';
 
 const App = () => {
+  const [{ token }, dispatch] = useStateProvider();
+
   useEffect(() => {
     const hash = window.location.hash;
     if (hash){
       const token = hash.substring(1).split('&')[0].split('=')[1];
-      console.log(token)
+      dispatch({action:reducerCases.SET_TOKEN, token})
     }
-  }, [])
+  }, [token, dispatch])
+  
   return (
     <div>
       <Login />
---
+++ src/components/Spotify.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Spotify = () => {
+  return (
+    <div>
+      Spotify
+    </div>
+  )
+}
+
+export default Spotify
