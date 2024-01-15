7b24232325ca3f33743c5dfeb1e402cc7c36f52a
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2024-01-15 23:23:58 +0700

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,33 @@ import '../styles/Footer.scss'
 
 const Footer = () => {
   return (
-    <div className='footer_container'>
-      Footer
-    </div>
+    <footer className="footer">
+      <div className="footer-section">
+        <h4>Company</h4>
+        <ul>
+          <li>About</li>
+          <li>Jobs</li>
+          <li>For the Record</li>
+          <li>Communities</li>
+        </ul>
+      </div>
+      <div className="footer-section">
+        <h4>For Artists</h4>
+        <ul>
+          <li>Developers</li>
+          <li>Advertising</li>
+          <li>Investors</li>
+          <li>Vendors</li>
+        </ul>
+      </div>
+      <div className="footer-section">
+        <h4>Useful links</h4>
+        <ul>
+          <li>Support</li>
+          <li>Free Mobile App</li>
+        </ul>
+      </div>
+    </footer>
   )
 }
 
