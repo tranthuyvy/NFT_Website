ab6f85efbb901617bf42b28255c5f41cdc139b30
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2024-01-15 11:58:22 +0700

    sidebar

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import '../styles/Footer.scss'
 
 const Footer = () => {
   return (
-    <div>
+    <div className='footer_container'>
       Footer
     </div>
   )
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import '../styles/Sidebar.scss'
+import { RiHomeSmileFill } from "react-icons/ri";
+import { MdOutlineLibraryMusic } from "react-icons/md";
+import { IoMdSearch } from "react-icons/io";
 
 const Sidebar = () => {
   return (
     <div className='sidebar_container'>
-      SideBar
+      <div className='top_links'>
+        <div className='logo'>
+          <img src='https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_Black.png' alt='Spotify' />
+        </div>
+        <ul>
+          <li>
+            <RiHomeSmileFill />
+            <span>Home</span>
+          </li>
+          <li>
+            <IoMdSearch />
+            <span>Search</span>
+          </li>
+          <li>
+            <MdOutlineLibraryMusic />
+            <span>Your Lyrik</span>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
